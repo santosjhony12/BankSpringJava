fef8d0e8f4feb15f6061a8f01a6726a53bfce0a2
santosjhony12 <santosdesouzajhony@gmail.com>
2023-09-08 10:11:10 -0300

    :docs: Organizando o repository git

--- src/main/java/com/bank/BankSpring/Model/Cliente/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bank.BankSpring.Model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends JpaRepository <Cliente, Long> {
-    Cliente findByCpf(String cpf);
-
-}
